d383e5b8f578b18e5285fe5a3abab4b07258cbef
Abubakar <abubakar@code-desk.com>
2016-03-30 13:57:18 +0500

    change config

--- config/app.php
+++ config/app.php
@@ -144,7 +144,6 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Dingo\Api\Provider\LaravelServiceProvider::class,
         Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
         Barryvdh\Cors\ServiceProvider::class,
 
