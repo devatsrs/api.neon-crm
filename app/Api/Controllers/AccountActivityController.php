59661dabda00a41ac0ea8064ba996c8dbffba60a
umer <umer.zafar@code-desk.com>
2016-04-26 20:07:56 +0500

    Account Email api Changes

--- app/Api/Controllers/AccountActivityController.php
+++ app/Api/Controllers/AccountActivityController.php
@@ -28,44 +28,7 @@ class AccountActivityController extends BaseController {
 	 * @return Response
 	 */
 
-    function sendtestmail()
-    {
-        $to = "umer.ahmed@code-desk.com";
-        $subject = "HTML email";
-
-        $message = "
-<html>
-<head>
-<title>HTML email</title>
-</head>
-<body>
-<p>This email contains HTML Tags!</p>
-<table>
-<tr>
-<th>Firstname</th>
-<th>Lastname</th>
-</tr>
-<tr>
-<td>John</td>
-<td>Doe</td>
-</tr>
-</table>
-</body>
-</html>
-";
-
-// Always set content-type when sending HTML email
-        $headers = "MIME-Version: 1.0" . "\r\n";
-        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-
-// More headers
-        $headers .= 'From: <noreply@wave-tel.com>' . "\r\n";
-
-
-        mail($to,$subject,$message,$headers);
-    }
     public function sendMail(){
-        $this->sendtestmail();
 		
         $data = Input::all();
         $rules = array(
