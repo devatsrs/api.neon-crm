3ad72731d631f0b8f6f43eccf0edf24db7d24462
umer <umer.zafar@code-desk.com>
2016-04-27 22:09:31 +0500

    Email Attachments bugs

--- app/Api/helpers.php
+++ app/Api/helpers.php
@@ -82,7 +82,7 @@ function sendMail($view,$data){
 				$Attachmenturl = Config::get('app.upload_path')."/".$attachment_data['filepath'];
 			}
             $path =    \App\AmazonS3::unSignedUrl($attachment_data['filepath']);
-            $file = getenv('TEMP_PATH').basename($path);
+            $file = getenv('TEMP_PATH').'/'.basename($path);
             file_put_contents($file,file_get_contents($path ));
 			\Illuminate\Support\Facades\Log::info($file);
 			$mail->AddAttachment($file,$attachment_data['filename']);
