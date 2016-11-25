2aacfdee67e239c66b8b08c9bd4a39cd1aec67be
umer <umer.zafar@code-desk.com>
2016-11-25 19:36:56 +0500

    Ticket table name changed

--- app/Api/models/Ticket.php
+++ app/Api/models/Ticket.php
@@ -8,7 +8,7 @@ class Ticket extends Model
 {
     protected $guarded = array("ID");
 
-    protected $table = 'tblAccountTickets';
+    protected $table = 'tblTickets';
 
     protected $primaryKey = "ID";
 }
