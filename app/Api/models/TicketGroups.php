25f82f421c1c378fe6789442a2e65cec316413f1
umer <umer.zafar@code-desk.com>
2017-03-07 20:29:32 +0500

    #NEON-75

--- app/Api/models/TicketGroups.php
+++ app/Api/models/TicketGroups.php
@@ -46,31 +46,6 @@ class TicketGroups extends \Eloquent
         }
     }
 	
-	static function get_group_agents($id,$select = 1,$fld = 'UserID')
-	{
-		if($select){
-			$Groupagents    =   array("Select"=>0);
-		}else{
-			$Groupagents    =   array();
-		}
-		if($id)
-		{
-			$Groupagentsdb	=	TicketGroupAgents::where(["GroupID"=>$id])->get(); 
-		}
-		else
-		{
-			$Groupagentsdb	=	TicketGroupAgents::get(); 
-		}
-		
-		foreach($Groupagentsdb as $Groupagentsdata){
-			$userdata = 	User::find($Groupagentsdata->UserID);
-			if($userdata){	
-				
-				$Groupagents[$userdata->FirstName." ".$userdata->LastName] =	$userdata->$fld; 
-			}			
-		} 
-		return $Groupagents;
-	}
 	
 	static function Get_User_Groups($id)
 	{
