ed061148a6a6f514ab05167c1dad7815f9df14ec
vishal <vishal,jagani@code-desk.com>
2018-01-25 13:27:48 +0530

    langPath change

--- app/Api/LibClass/DomainApplication.php
+++ app/Api/LibClass/DomainApplication.php
@@ -2,6 +2,6 @@
 
 class DomainApplication extends \Illuminate\Foundation\Application {
     public function langPath() {
-        return 'D:\www\vishal\neon\web\vishal\app\lang';
+        return __DIR__.'/../../../../../app/lang';
     }
 }
